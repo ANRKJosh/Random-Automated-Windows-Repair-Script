9e73d4ec12986f7cecfa4d4224bfa7e49ca031a7
ANRKJosh <joshuab1@live.co.uk>
2021-03-30 14:40:38 +0100

    Add files via upload

---
+++ RepairV1.ps1
@@ -0,0 +1,20 @@
+﻿mkdir C:\Xuper
+$LogFile = "C:\Xuper\RepairScript.txt"
+$Date = Get-Date
+
+"Repair Log" | Out-File $LogFile -Force -Append
+$Date | Out-File $LogFile -Force -Append
+"" | Out-File $LogFile -Force -Append
+
+"Running SFC" | Out-File $LogFile -Force -Append
+sfc /scannow | Out-File $LogFile -Force -Append
+
+"" | Out-File $LogFile -Force -Append
+
+"Running CHKDSK /scan" | Out-File $LogFile -Force -Append
+chkdsk /scan | Out-File $LogFile -Force -Append
+
+"" | Out-File $LogFile -Force -Append
+
+"Running DISM" | Out-File $LogFile -Force -Append
+dism /online /cleanup-image /restorehealth | Out-File $LogFile -Force -Append
